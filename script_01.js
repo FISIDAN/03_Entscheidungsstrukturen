0a4b8f6f842169678d273a1f570fc1ec1cd79c50
FISIDAN <schwark@posteo.de>
2023-01-16 11:44:22 +0100

    Test logische Aussage

--- script_01.js
+++ script_01.js
@@ -4,10 +4,18 @@
 
 // Deklaration + Assigment
 const ageJohn = 35;
-const ageMark = 30;
+const ageMark = 35;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
 // Test logische Aussage
 isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
